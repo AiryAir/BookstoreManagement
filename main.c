86be0fb4683048fee8d3570a81ea60d36f7d0561
Pratham <69145482+AiryAir@users.noreply.github.com>
2022-04-09 15:25:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+
+   Functions:
+
+
+/
+   add to read (done)
+   add to unread (done)
+   seaarch using name (done)
+   find highest rated
+   add form unread to read 
+   delete from read 
+   delete form unread
+
+
+    */
+
 void addRead();
-void addUnead();
-void search(char sk[30]);
+void addUnread();
+void search(char sk[30]);   
+void deleteUnread(char sk[30]);
+
+
 
 struct read{
 
@@ -28,25 +48,10 @@ int main(){
 
     char sk[30]="kriti";
 
-    //addRead(); addRead();
-    search(sk);
+    //addUnread(); addUnread();
+    deleteUnread(sk);
 
-    /*
-
-   Functions:
-
-
-/
-   add to read (done)
-   add to unread (done)
-   seaarch using name
-   find highest rated
-   add form unread to read 
-   delete from read 
-   delete form unread
-
-
-    */
+    
 }
 
 void search(char sk[30]){
@@ -67,10 +72,82 @@ void search(char sk[30]){
       }
     }
 
-   fclose (inf);
-   
+   fclose (inf);   
 
 }
+
+
+void deleteUnread(char sk[30]){
+
+
+
+    struct unread books[100];
+    int counter=0;
+
+    FILE *inf;
+    struct unread inp;
+
+    inf = fopen ("unread.txt", "r");
+    if (inf == NULL) {
+        fprintf(stderr, "\nError to open the file\n");
+        //exit (1);
+    }
+
+    while(fread(&inp, sizeof(struct unread), 1, inf)){
+      printf ("name = %s\n", inp.name);
+      strcpy(books[counter].name,inp.name); strcpy(books[counter].author,inp.author); strcpy(books[counter].genre,inp.genre);
+      counter++;
+    }
+
+   fclose (inf);
+
+   for(int i=0; i<10;i++){
+       printf("%s %s %s\n",books[i].name,books[i].author,books[i].genre);
+
+   }
+
+//     FILE *outfile;
+     
+//     // open file for writing
+//     outfile = fopen ("unread.txt", "w");
+//     if (outfile == NULL)
+//     {
+//         fprintf(stderr, "\nError opening file\n");
+//         //exit (1);
+//     }
+
+//     //struct unread b;
+
+//    /* printf("Enter the details of the book you just read in the format below\n");
+//     printf("Name\n"); scanf("%s",&b.name);
+//     printf("Author\n"); scanf("%s",&b.author);
+//     printf("genre\n"); scanf("%s", &b.genre);
+
+//     */
+     
+//     // write struct to file
+
+//     int length= sizeof(books)/sizeof(struct unread);
+
+//     for(int i=0;i<length;i++){
+
+//     if(strcmp(sk,books[i].name)){
+
+//         fwrite(&books[i], sizeof(struct unread) ,1 ,outfile);
+
+//     }   
+
+//     } 
+     
+
+//     // close file
+//     fclose (outfile);
+
+
+}
+
+
+
 
 void addRead(){
 
@@ -81,7 +158,7 @@ void addRead(){
     if (outfile == NULL)
     {
         fprintf(stderr, "\nError opening file\n");
-        exit (1);
+        //exit (1);
     }
 
     char name[30], author[30], genre[20]; int stars;
@@ -103,53 +180,52 @@ void addRead(){
     // write struct to file
     fwrite(&a, sizeof(struct read) ,1 ,outfile);
      
-    if(fwrite != 0)
+    if(fwrite != 0){
         printf("contents to file written successfully !\n");
-    else
+    }
+    else{
         printf("error writing file !\n");
+    }
  
     // close file
     fclose (outfile);
  
-    return 0;
+    return;
 
 }
-/*
+
 
 void addUnread(){
 
-     FILE *outfile;
+    FILE *outfile;
      
     // open file for writing
     outfile = fopen ("unread.txt", "a");
     if (outfile == NULL)
     {
         fprintf(stderr, "\nError opening file\n");
-        exit (1);
+        //exit (1);
     }
 
-    char name[30], author[30], genre[20]; 
+    struct unread b;
 
-    printf("Enter the details of the book you just read in the format below\n");
-    printf("Name\n"); scanf("%s",name);
-    printf("Author\n"); scanf("%s",author);
-    printf("genre\n"); scanf("%s", genre);
 
-    struct unread a={name,author,genre};
+    printf("Enter the details of the book you want to read\n");
+    printf("Name\n"); scanf("%s",&b.name);
+    printf("Author\n"); scanf("%s",&b.author);
+    printf("genre\n"); scanf("%s", &b.genre);
      
     // write struct to file
-    fwrite(&a, sizeof(struct unread) ,1 ,outfile);
+    fwrite(&b, sizeof(struct unread) ,1 ,outfile);
      
-    if(fwrite != 0)
+    if(fwrite != 0){
         printf("contents to file written successfully !\n");
-    else
+    }
+    else{
         printf("error writing file !\n");
+    }
  
     // close file
     fclose (outfile);
- 
-    return 0;
 
 }
-
-*/
